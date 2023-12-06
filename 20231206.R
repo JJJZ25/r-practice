7024f36bf541af08ea27456577b7e108b7e397f8
JJJZ25 <144878743+JJJZ25@users.noreply.github.com>
2023-12-06 14:24:57 +0800

    Add files via upload

---
+++ 20231206.R
@@ -0,0 +1,23 @@
+# a block of program
+# code block
+
+# example of a code block,
+# programming block
+{
+  # code lines
+}
+ 
+# use it repetitively
+# add for(...) in front of it
+for(.x in ...)
+{
+  # code lines
+}
+
+# use the block somewhere else
+# add function(...)
+# and bind it with a name
+myfun <- function(...)
+{
+  
+}
